13f5e383856691073ebfd29a3b79285208209daf
jiujudonggong <464983934@qq.com>
2023-03-13 14:57:58 +0800

    first commit

---
+++ src/main/java/com/example/demo/FoxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxDemoApplication.class, args);
+    }
+
+}
